2b1b3e478615328618c447dd7771218138fb9c16
Tarun Kushwaha <tarunkushwaha0809@gmail.com>
2026-06-04 02:00:26 +0530

    commit

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ import { cn } from '@/lib/utils';
 
 const navItems = [
   { href: '/', labelKey: 'nav.home', icon: Home },
-  { href: '/about', labelKey: 'About', icon: Info },
+  { href: '/about', labelKey: 'nav.about', icon: Info },
   { href: '/career', labelKey: 'nav.career', icon: Map },
   { href: '/scholarships', labelKey: 'nav.scholarships', icon: GraduationCap },
   { href: '/community', labelKey: 'nav.community', icon: Users },
